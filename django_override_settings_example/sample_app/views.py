a05ed9cb4549b798b2fe164f689fc70e1eb384d5
mrabedini <abedini.mohamadreza@gmail.com>
2019-04-14 07:06:46 +0430

    [sample_app]: a view that uses APP_SAMPLE_CONFIG was added.

--- django_override_settings_example/sample_app/views.py
+++ django_override_settings_example/sample_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .config import APP_SAMPLE_CONFIG
 # Create your views here.
+
+def sample_view(request):
+    return HttpResponse(APP_SAMPLE_CONFIG)
+    
+
